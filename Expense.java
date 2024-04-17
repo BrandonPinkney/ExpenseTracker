06796b4c1409b04e642b5bb48f7a4bd15a9848b4
BrandonPinkneyy <Pinkneybr@hotmail.com>
2024-04-17 01:06:22 -0400

    k

--- Expense.java
+++ Expense.java
@@ -1,7 +1,37 @@
+// expense class represeting individual expense , encapsulates the data with each expense
 
-// expense class 
+import java.sql.Date;
 
- // test 2 
 public class Expense {
+
+    String expenseMonth;
+    String description,category,paymentMethod;
+    double amount;
+   
+
+    Expense(String expenseMonth,double amount,String category,String description,String paymentMethod){
+        this.expenseMonth=expenseMonth;
+        this.description = description;
+        this.amount = amount;
+        this.category = category;
+        this.paymentMethod = paymentMethod;
+    }
+
+    public String getDescription(){
+        return description;
+    }
+    public String getCategiory(){
+        return category;
+    }
+    public String getPaymentMethod(){
+        return paymentMethod;
+    }
+    public String getExpenseMonth(){
+        return expenseMonth;
+    }
+    public double getAmount(){
+        return amount; 
+    }
     
-}
+    }
+
--- ExpenseManager.java
+++ ExpenseManager.java
@@ -1,6 +1,37 @@
-
-// holds main logic behind expenese tables
+import java.util.ArrayList;
+import java.util.List;
 
 public class ExpenseManager {
+
+    List<Expense> expenses = new ArrayList<>();
+
+    // adds an expense to list
     
-}
+    public void addExpense(Expense expense){
+         
+        expenses.add(expense);
+    }
+
+    // remove an expense from the list
+
+  public void removeExpense(Expense expense){
+
+    expenses.remove(expense);
+
+  }
+
+  public void expensesByMonth(String month){
+    List <Expense> months = new ArrayList<>();
+
+    for(Expense e: expenses){
+        if(e.getExpenseMonth().equals(month)){
+
+            months.add(e);
+        }
+   }
+  }
+
+  }
+
+    
+
