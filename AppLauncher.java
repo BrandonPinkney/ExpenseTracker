36e1d405af72bfd0f883d29fd2fdec6720e3838b
BrandonPinkney <124821589+BrandonPinkney@users.noreply.github.com>
2024-06-06 20:57:51 -0400

    Update AppLauncher.java

--- AppLauncher.java
+++ AppLauncher.java
@@ -7,11 +7,10 @@ public class AppLauncher {
         SwingUtilities.invokeLater(new Runnable() {
             @Override
             public void run() {
-                // display weather app gui
+                
                 new ExpenseGUI().setVisible(true);
 
-                // System.out.println(WeatherApp.getLocationData("Tokyo"));
-                // System.out.println(WeatherApp.getCurrentTime());
+                
             }
         });
     }
